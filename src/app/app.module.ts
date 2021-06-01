2dfd96aae8f1f91f24708f6fd8f2cf3ad079d259
takurofukuta <mojuraspax@icloud.com>
2021-06-01 19:43:18 +0900

    ItemListコンポーネントを作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { ItemListComponent } from './item-list/item-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, ItemListComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
