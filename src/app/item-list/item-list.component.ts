efdeec61717c67bd35d23a4ae7e6544f2e3654a9
takurofukuta <mojuraspax@icloud.com>
2021-06-01 20:13:04 +0900

    ngFor、ngIfの練習

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Item } from '../item.interface';
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.css']
+  styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent implements OnInit {
+  items: Item[] = [
+    {
+      id: 1,
+      name: 'ポンチョ',
+      price: 3000,
+      description: '',
+    },
+    {
+      id: 2,
+      name: 'セーター',
+      price: 5000,
+      description: 'ほどよくゆったりとしたシルエットでラフな着用感',
+    },
+    {
+      id: 3,
+      name: 'ダウンジャケット',
+      price: 6000,
+      description: '風を通さず、暖かさをキープ',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
